4d5ae41111f515bd385efa5ce73145eea57e6f35
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-16 12:02:52 +0300

    Add DOM load event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+//wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // select the form element
+  const form = document.getElementById('create-task-form')
+
+  //add a submit event listener to the form
+
 });
