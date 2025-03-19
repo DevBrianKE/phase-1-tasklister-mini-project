f6753f562fab1e8f84ac48f2bf91c7deb3a8ad18
DevBrianKE <kipchumbabrian47@gmail.com>
2025-03-19 19:18:10 +0300

    feat: prevent form submission and capture task input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ // Select the form element
+ const taskform = document.getElementById("create-task-form")
+
+
+  // Add a submit event listener
+  taskform.addEventListener("submit", function(event) {
+    event.preventDefault()// Prevent form from refreshing the page
+
+    // Get the value of the task input field
+    const taskInput = document.getElementById("new-task-description").value
+
+    console.log(task)
+  })
 });
